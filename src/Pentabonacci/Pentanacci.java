aee983253f3c7ddc6b5eceb5616756993331affa
alekseiLazunin <gegcuk@gmail.com>
2025-01-09 11:47:47 +0000

    feat: Implement Pentanacci sequence with odd count calculation
    - Added method `countOddPentaFib` to calculate the number of odd terms in the Pentanacci sequence up to a given index `n`.
    - Utilized a dynamic programming approach with a results array to store intermediate values for efficient computation.
    - Integrated modular arithmetic to identify odd numbers in the sequence.
    - Added a `main` method with test cases for verifying the functionality with various input values.

--- src/Pentabonacci/Pentanacci.java
+++ src/Pentabonacci/Pentanacci.java
@@ -1,4 +1,35 @@
 package Pentabonacci;
 
+import java.util.Arrays;
+
 public class Pentanacci {
+    public static long countOddPentaFib(long n) {
+        long result = 0;
+        int[] resultsArray = new int[(int) n + 1];
+        resultsArray[0] = 0;
+        resultsArray[1] = 1;
+        resultsArray[2] = 1;
+        resultsArray[3] = 0;
+        resultsArray[4] = 0;
+        for(int i = 5; i <= n; i++){
+            int temp = 0;
+            for(int j = i; j >= i - 5; j-- ){
+                temp += resultsArray[j];
+            }
+            resultsArray[i] = (temp % 2 == 0) ? 0 : 1;
+        }
+
+        result = Arrays.stream(resultsArray).sum();
+
+        return result-1;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Pentanacci.countOddPentaFib(45));
+        System.out.println(Pentanacci.countOddPentaFib(68));
+        System.out.println(Pentanacci.countOddPentaFib(76));
+        System.out.println(Pentanacci.countOddPentaFib(100));
+        System.out.println(Pentanacci.countOddPentaFib(121));
+    }
 }
