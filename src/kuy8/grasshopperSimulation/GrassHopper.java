adb1a7c87392e8df2a93a274255ea17dcf9a60d6
alekseiLazunin <gegcuk@gmail.com>
2025-01-11 13:02:28 +0000

    Grasshopper is solved

---
+++ src/kuy8/grasshopperSimulation/GrassHopper.java
@@ -0,0 +1,10 @@
+package kuy8.grasshopperSimulation;
+
+public class GrassHopper {
+
+    public static int summation(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++) sum+=i;
+        return sum;
+    }
+}
