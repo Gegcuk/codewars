5831078bdc0642dcac64780656bb91c2c8f8d44a
alekseiLazunin <gegcuk@gmail.com>
2025-01-11 13:03:52 +0000

    Grasshopper is optimized

--- src/kuy8/grasshopperSimulation/GrassHopper.java
+++ src/kuy8/grasshopperSimulation/GrassHopper.java
@@ -3,8 +3,6 @@ package kuy8.grasshopperSimulation;
 public class GrassHopper {
 
     public static int summation(int n) {
-        int sum = 0;
-        for(int i = 1; i <= n; i++) sum+=i;
-        return sum;
+        return  n*(n+1)/2;
     }
 }
