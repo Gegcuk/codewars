bf0d691c97f0923c434640ecb1d487491d05df33
alekseiLazunin <gegcuk@gmail.com>
2025-01-11 12:51:07 +0000

    Sum of positive solved

---
+++ src/sumOfPositive/Positive.java
@@ -0,0 +1,11 @@
+package sumOfPositive;
+
+import java.util.Arrays;
+
+public class Positive{
+
+    public static int sum(int[] arr){
+        return Arrays.stream(arr).filter(x -> x > 0).sum();
+    }
+
+}
